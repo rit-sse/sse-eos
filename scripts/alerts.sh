743e87b403faecf549734970221cb406577202fb
Michael Timbrook <timbrook480@gmail.com>
2016-03-15 17:22:53 -0400

    Email!

--- scripts/alerts.sh
+++ scripts/alerts.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker events;
+while read event;
+do
+  echo -e "Subject:Service has started\n\n$event\n" | sendmail thisisprod@timbrook.im
+done < <(docker events --filter event=start);
