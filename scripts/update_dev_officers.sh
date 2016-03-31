27c848079c2e683a46e3c7bdca33efe1161da66e
Michael Timbrook <timbrook480@gmail.com>
2016-03-31 14:27:34 -0400

    Updated script

--- scripts/update_dev_officers.sh
+++ scripts/update_dev_officers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker cp ./add_dev_person.sql postgres:/tmp/script.sql;
-docker exec psql -U postgres -f /tmp/script.sql sse;
+docker exec postgres psql -U postgres -f /tmp/script.sql sse;
 
