2b2cf7d73a689e0f23b50a56b1646bd1f1b4f64d
Michael Timbrook <timbrook480@gmail.com>
2016-04-19 16:03:56 -0400

    added users to dev

--- scripts/update_dev_officers.sh
+++ scripts/update_dev_officers.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-docker cp /home/deploy/sse-eos/scripts/add_dev_person.sql postgres:/tmp/script.sql;
+ACCOUNTS="mpt2360
+jwd6999
+tbm2961";
+
+rm /tmp/insert.sql;
+
+for name in $ACCOUNTS;
+do
+	sed "s/mpt2360/$name/g" ./scripts/add_dev_person.sql >> /tmp/insert.sql;
+done;
+
+docker cp /tmp/insert.sql postgres:/tmp/script.sql;
 docker exec postgres psql -U postgres -f /tmp/script.sql sse;
 
