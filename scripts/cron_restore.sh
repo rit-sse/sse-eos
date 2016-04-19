a37d0dfd9bc3a122e9671b3676f042f28a06c6f7
Michael Timbrook <timbrook480@gmail.com>
2016-04-19 14:56:43 -0400

    scripts

--- scripts/cron_restore.sh
+++ scripts/cron_restore.sh
@@ -2,3 +2,4 @@
 NAME=postgres
 docker cp /home/deploy/backups/sse_backup.tar $NAME:/tmp/
 docker exec $NAME pg_restore --username "sse" --dbname "sse" --no-password  --section pre-data --section data --section post-data --clean --verbose "/tmp/sse_backup.tar"
+/home/deploy/sse-eos/scripts/update_dev_officers.sh
