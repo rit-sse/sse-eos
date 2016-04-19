0626032cb631295ffa1599d7c1a72737bf08c389
Michael Timbrook <timbrook480@gmail.com>
2016-04-19 14:42:03 -0400

    Script changes

--- scripts/cron_backup.sh
+++ scripts/cron_backup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# docker rm backup_ambassador;
-# docker run -d -p 5432:5432 --link docker_postgres_1 --name backup_ambassador svendowideit/ambassador;
-NAME=docker_postgres_1
+NAME=postgres
 docker exec $NAME pg_dump --username "sse" --no-password  --format tar --blobs --section pre-data --section data --section post-data --verbose --file "/tmp/sse_backup.tar" "sse"
-docker cp $NAME:/tmp/sse_backup.tar ./
-scp ./sse_backup.tar sseprod.se.rit.edu:/root/backups/
+docker cp $NAME:/tmp/sse_backup.tar /home/deploy/backups/
+scp /home/deploy/backups/sse_backup.tar deploy@ssedev.se.rit.edu:/home/deploy/backups/
 docker exec $NAME rm /tmp/sse_backup.tar
-rm ./sse_backup.tar
+
