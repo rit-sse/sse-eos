916f9edb6e9507666f2980fc550dee8dba939683
Michael Timbrook <timbrook480@gmail.com>
2016-04-19 16:24:29 -0400

    Fixed byte order in template file

--- scripts/add_dev_person.sql
+++ scripts/add_dev_person.sql
@@ -1,3 +1,1 @@
-﻿INSERT INTO public.officers(
-          title, email, "primaryOfficer", "userDce", "startDate", "endDate", "createdAt", "updatedAt", "committeeName"
-) VALUES ('Tech Dev', 'techdev', true, 'mpt2360', '2016-01-01', '2017-01-01', '2016-01-01', '2016-01-01', 'Technology');
+INSERT INTO public.officers(title, email, "primaryOfficer", "userDce", "startDate", "endDate", "createdAt", "updatedAt", "committeeName") VALUES ('Tech Dev', 'techdev', true, 'mpt2360', '2016-01-01', '2017-01-01', '2016-01-01', '2016-01-01', 'Technology');
--- scripts/update_dev_officers.sh
+++ scripts/update_dev_officers.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-ACCOUNTS="mpt2360
-jwd6999
-tbm2961";
+ACCOUNTS=("mpt2360" "jwd6999" "tbm2961");
 
 rm /tmp/insert.sql;
 
-for name in $ACCOUNTS;
+for name in "${ACCOUNTS[@]}";
 do
-	sed "s/mpt2360/$name/g" ./scripts/add_dev_person.sql >> /tmp/insert.sql;
+	echo "Added user $name to insert script";
+	sed -e "s/mpt2360/$name/g" ./scripts/add_dev_person.sql >> /tmp/insert.sql;
 done;
 
 docker cp /tmp/insert.sql postgres:/tmp/script.sql;
