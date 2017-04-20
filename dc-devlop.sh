54c4174157d43df463f19f98a828de2bf6d972bc
Michael Timbrook <timbrook480@gmail.com>
2017-04-20 13:38:25 -0400

    Cleaned for the shinny new

--- dc-devlop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker-compose -f docker-compose.devlop.yml -f docker-compose.override.yml $@
--- scripts/cron_restore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-NAME=postgres
-docker cp /home/deploy/backups/sse_backup.tar $NAME:/tmp/
-docker exec $NAME pg_restore --username "sse" --dbname "sse" --no-password  --section pre-data --section data --section post-data --clean --verbose "/tmp/sse_backup.tar"
-/home/deploy/sse-eos/scripts/update_dev_officers.sh
