6d341b2fb6ae00bda2da6762e7d4a64734c91afd
Michael Timbrook <timbrook480@gmail.com>
2017-04-27 11:34:12 -0400

    Added AB with old go service. Switched to the underlying swarm loadbalancer.

--- bootstrap_swarm.sh
+++ bootstrap_swarm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "I'm assuming you set the db label on the host you want...";
+
 # Docker networks
 docker network create -d overlay external
 docker network create -d overlay internal
@@ -7,3 +9,14 @@ docker network create -d overlay internal
 # You should make sure the following labels exists on the right nodes
 # db=true
 # docker node update --label-add db=true moby
+
+cd loadbalancer && ./deploy
+cd ..
+cd sseprod.se.rit.edu && HOST=sseprod.se.rit.edu ./deploy
+cd ..
+cd sseold && HOST=sseprod.se.rit.edu ./deploy
+cd ..
+cd allendoesphysics.com && HOST=allendoesphysics.com ./deploy
+cd ..
+cd database && ./deploy
+cd ..
