06086e143a5f6198ec5d5d96415b83df84ecd497
Dylan Vuz <vuzdylan@gmail.com>
2017-09-18 20:13:58 -0400

    updated some of the start up files

--- bootstrap_swarm.sh
+++ bootstrap_swarm.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
+# IMPORTANT
+# You should make sure the following labels exists on the right nodes
+# db=true
+# docker node update --label-add db=true moby
 echo "I'm assuming you set the db label on the host you want...";
 
 # Docker networks
 docker network create -d overlay external
 docker network create -d overlay internal
 
-# You should make sure the following labels exists on the right nodes
-# db=true
-# docker node update --label-add db=true moby
+# This file is only for bringing up the base site
 
 cd loadbalancer && ./deploy
 cd ..
-cd sseprod.se.rit.edu && HOST=sseprod.se.rit.edu ./deploy
-cd ..
-cd sseold && HOST=sseprod.se.rit.edu ./deploy
-cd ..
-cd allendoesphysics.com && HOST=allendoesphysics.com ./deploy
-cd ..
 cd database && ./deploy
 cd ..
-cd planyourcourses.com && HOST=planyourcourses.com ./deploy
-cd ..
+cd sse.rit.edu && HOST=sse.rit.edu ./deploy
